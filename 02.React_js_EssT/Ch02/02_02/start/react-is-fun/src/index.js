1e3e0bdf67f9f119accff977b7daf0094b0a80d2
FirozKabir65 <firoj.kabir007@gmail.com>
2020-04-12 16:35:21 +0200

    React essential training: chapter 02 done

--- 02.React_js_EssT/Ch02/02_02/start/react-is-fun/src/index.js
+++ 02.React_js_EssT/Ch02/02_02/start/react-is-fun/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+var style = {
+	backgroundColor: '#221b24',
+	color: '#6d8d89',
+	fontFamily: 'Arial'
+}
+
+//chapter 02-03
+// const title = React.createElement(
+// 	'h1',
+//   	{id: 'title', className: 'header', style: style},
+//   	'Hello World'
+// )
+
+// ReactDOM.render(
+// 	title,
+// 	document.getElementById('root')
+// )
+
+//chapter 02-04
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<div style={style}>
+		<h1>Hello World</h1>
+		<p>We're glad to here.</p>
+	</div>,
+	document.getElementById('root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
